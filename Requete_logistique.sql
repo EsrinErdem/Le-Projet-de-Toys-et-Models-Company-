189a669695218161c785ed9a02c68d2ac03465ae
Esrin ERDEM <esrin.sari@gmail.com>
2022-06-26 18:23:24 +0200

    Update Requete_logistique.sql
    The stock of the 5 most ordered products.

--- Requete_logistique.sql
+++ Requete_logistique.sql
@@ -1,4 +1,4 @@
-SELECT products.productName, products.quantityInStock, SUM(orderdetails.quantityOrdered)
+SELECT products.productName, products.quantityInStock, sum(orderdetails.quantityOrdered)
 FROM orderdetails
 INNER JOIN products ON products.productCode = orderdetails.productCode
 GROUP BY orderdetails.productCode
